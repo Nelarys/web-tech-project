af6cca459917ee070acc12f7278e0a0d3d123c87
RuneHeus <83946705+RuneHeus@users.noreply.github.com>
2024-10-22 10:39:49 -0400

    Mysql 2 enzo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-var mysql = require('mysql');
+var mysql = require('mysql2');
 
 const app = express();
 const PORT = 3000;
