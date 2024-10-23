563700e2d3064ef7cc674790a901e74119b6af48
RuneHeus <83946705+RuneHeus@users.noreply.github.com>
2024-10-23 09:54:18 -0400

    Added account creation + loggin sessions

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+
+const session = require('express-session'); //Used for session control (user login/logout)
+
 require('dotenv').config();
 var mysql = require('mysql2');
 
@@ -7,6 +10,13 @@ const app = express();
 const PORT = 3000;
 
 const API_KEY = process.env.API_KEY;
+
+app.use(session({
+  secret: '2002-03-04-05',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
@@ -22,10 +32,38 @@ app.get('/', (req, res) => {
 });
 
 app.get('/account', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'Account', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'Account', 'login.html'));
 });
 
-app.post('/api/login', (req, res) => {
+
+//For creating a user session (login)
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).send('Username and password are required');
+  }
+
+  const sql = 'SELECT id FROM Users WHERE username = ? AND password = ?';
+
+  con.query(sql, [username, password], (err, results) => {
+    if (err) {
+      console.error('Error querying the database:', err);
+      return res.status(500).send('Internal server error');
+    }
+
+    if (results.length > 0) {
+      const userId = results[0].id;
+      req.session.userId = userId;
+      res.redirect('/');
+    } else {
+      res.status(401).send('Invalid username or password');
+    }
+  });
+});
+
+//For account creation
+app.post('/create-account', (req, res) => {
 
   const username = req.body.username;
   const password = req.body.password;
@@ -34,14 +72,14 @@ app.post('/api/login', (req, res) => {
     return res.status(400).send("Username and password are required");
   }
 
-  var sql = "INSERT INTO Users (id, username, password, bio) VALUES (10, ?, ?, 'Dit is een test bio')";
+  const sql = "INSERT INTO Users (username, password, bio) VALUES (?, ?, 'bio')";
   con.query(sql, [username, password], function (err, result) {
     if (err) {
       console.error("Error inserting data:", err);
       return res.status(500).send("Error inserting data");
     }
     console.log("1 record inserted");
-    res.sendFile(path.join(__dirname, 'public', 'Account', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'Account', 'login.html'));
   });
 });
 
